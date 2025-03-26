2c28f52d7810894c67fb63a782e9ecf202c360a1
ivonamlinarevic <imlina00@fesb.hr>
2025-03-26 17:03:56 +0100

    added frontend folder

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Pozdrav od Express poslužitelja!");
+   });
+
+const PORT = 3000;
+  app.listen(PORT, () => {
+   console.log(`Server sluša zahtjeve na portu ${PORT}`);
+  });
+
+app.use(express.json());
